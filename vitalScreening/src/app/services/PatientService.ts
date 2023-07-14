85c03e259acc127fededcdf2be550223cf14aae4
tmugaro <naldotinashe1@gmail.com>
2023-07-14 13:13:22 -0400

    added vitals service for adding and updating and delete

--- vitalScreening/src/app/services/PatientService.ts
+++ vitalScreening/src/app/services/PatientService.ts
@@ -29,25 +29,96 @@ deletePatientUrl : string;
 
 constructor(private http : HttpClient) {
 
-  this.addPatientURL = '/emp/addPatient';
-  this.addWeightURL = '/emp/addWeight';
-  this.addHeightURL = '/emp/addHeight';
-  this.addBloodGlucoseURL = '/emp/addBloodGlucose';
-  this.addBloodPressureURL = '/emp/addBloodPressure';
-  this.getWeightURL = '/emp/getWeight';
-  this.getPatientURL = '/emp/getPatient';
-  this.getHeightURL = '/emp/getHeight';
-  this.getBloodGlucoseURL = '/emp/getBloodGlucose';
-  this.getBloodPressureURL = '/emp/getBloodPressure';
-  this.updateWeightUrl = '/emp/updateWeight';
-  this.updateHeightUrl = '/emp/updateHeight';
-  this.updateBloodPressureUrl = '/emp/updateBloodPressure';
-  this.updateBloodGlucoseUrl = '/emp/updateBloodGlucose';
-  this.deleteeWeightUrl = '/emp/deleteWeightById';
-  this.deleteHeightUrl = '/emp/deleteHeightById';
-  this.deleteBloodPressureUrl = '/emp/deleteBloodPressureById';
-  this.deleteBloodGlucoseUrl = '/emp/deleteBloodGlucoseById';
-  this.deletePatientUrl = '/emp/deletePatientById';
+  this.addPatientURL = '/vital/addPatient';
+  this.addWeightURL = '/vital/addWeight';
+  this.addHeightURL = '/vital/addHeight';
+  this.addBloodGlucoseURL = '/vital/addBloodGlucose';
+  this.addBloodPressureURL = '/vital/addBloodPressure';
+  this.getWeightURL = '/vital/getWeight';
+  this.getPatientURL = '/vital/getPatient';
+  this.getHeightURL = '/vital/getHeight';
+  this.getBloodGlucoseURL = '/vital/getBloodGlucose';
+  this.getBloodPressureURL = '/vital/getBloodPressure';
+  this.updateWeightUrl = '/vital/updateWeight';
+  this.updateHeightUrl = '/vital/updateHeight';
+  this.updateBloodPressureUrl = '/vital/updateBloodPressure';
+  this.updateBloodGlucoseUrl = '/vital/updateBloodGlucose';
+  this.deleteeWeightUrl = '/vital/deleteWeightById';
+  this.deleteHeightUrl = '/vital/deleteHeightById';
+  this.deleteBloodPressureUrl = '/vital/deleteBloodPressureById';
+  this.deleteBloodGlucoseUrl = '/vital/deleteBloodGlucoseById';
+  this.deletePatientUrl = '/vital/deletePatientById';
 
  }
+
+ addPatient(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.addPatientURL,patient);
+  }
+
+  addWeight(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.addWeightURL,patient);
+  }
+
+  addHeight(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.addHeightURL,patient);
+  }
+
+  addBloodGlucose(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.addBloodGlucoseURL,patient);
+  }
+  
+  addBloodPressure(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.addBloodPressureURL,patient);
+  }
+
+  getWeight(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.getWeightURL,patient);
+  }
+
+  getPatient(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.getPatientURL,patient);
+  }
+
+  getBloodGlucose(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.getBloodGlucoseURL,patient);
+  }
+
+  getBloodPressure(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.updateBloodPressureUrl,patient);
+  }
+
+  updateWeight(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.updateWeightUrl,patient);
+  }
+
+  updateBloodPressure(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.updateBloodPressureUrl,patient);
+  }
+
+  updateBloodGlucose(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.updateBloodGlucoseUrl,patient);
+  }
+  
+  deleteeWeight(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.deleteeWeightUrl,patient);
+  }
+
+  deleteBloodPressure(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.deleteBloodPressureUrl,patient);
+  }
+
+  deleteBloodGlucose(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.deleteBloodGlucoseUrl,patient);
+  }
+
+  deletePatient(patient : Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.deletePatientUrl,patient);
+  }
+
+ 
+
+
+
+
+
 }
