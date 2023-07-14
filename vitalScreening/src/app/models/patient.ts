7524bcd269cc8ae9120703de7c54a2db5a9c7746
tmugaro <naldotinashe1@gmail.com>
2023-07-14 12:17:58 -0400

    added patient entity and angular structure

---
+++ vitalScreening/src/app/models/patient.ts
@@ -0,0 +1,15 @@
+export class Patient {
+    id : number=0;
+    firstName : string='';
+    lastName : string ='';
+    gender : string ='';
+    //dateOfBirth: Date = ;
+    phoneNumber: number =0;
+    height : number=0;
+    weight: number=0;
+    bloodGlucose: number=0;
+    systolicBloodPressure: number=0;
+    diastolicBloodPressure: number=0;
+    emailAddress : string='';
+   
+}
---
+++ vitalScreening/src/app/vitals_screening.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms'
+
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './vitals_screening.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class VitalsScreeningComponent {
+
+  
+}
